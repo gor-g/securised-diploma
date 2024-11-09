9ebabc5ba2de59ad533e17b73fba5b8aca1929eb
Gor G <gg@gorg.fr>
2024-11-09 21:20:16 +0100

    added test.sh
    Co-authored-by: JulienGln <JulienGln@users.noreply.github.com>

---
+++ test.sh
@@ -0,0 +1,6 @@
+python3 main.py insert data/diplome-BG.png "Le mystère des Pyramides, c’est le mystère de la conscience dans laquelle on n’entre pas." data/tmp/diplome-BG_msg.png
+python3 main.py extract data/tmp/diplome-BG_msg.png
+python3 main.py text ./data/diplome-BG.png "Ceci est du texte" ./data/tmp/output-text.png
+python3 main.py keygen 2048 "jg"
+python3 main.py create "Truc BIDULE" "11/11/1111" 2024 14.65 "bien"
+python3 main.py verify "Truc BIDULE"
